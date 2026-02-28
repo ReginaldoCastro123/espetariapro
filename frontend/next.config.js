aac9cb84ee3e5526165e024cfd8f456db664f9b0
Reginaldo <reginaldodasilvacastro4@gmail.com>
2026-02-28 00:54:24 -0300

    update teste

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig = {
-  // A propriedade 'experimental' com 'appDir' foi removida porque é padrão no Next.js 14
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        // IMPORTANTE: Em produção, o localhost não existe na Vercel!
-        // Use a variável de ambiente para que ele saiba onde está seu Render
         destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3333'}/api/:path*`,
       },
     ];
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
